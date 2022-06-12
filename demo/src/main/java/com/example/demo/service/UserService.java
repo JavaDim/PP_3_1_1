8106e88d386a100659b58339bb81abdb89be9654
Дим <102228796+JavaDim@users.noreply.github.com>
2022-06-13 00:18:32 +0500

    Add files via upload

--- demo/src/main/java/com/example/demo/service/UserService.java
+++ demo/src/main/java/com/example/demo/service/UserService.java
@@ -2,6 +2,7 @@ package com.example.demo.service;
 
 import com.example.demo.dao.UserRepository;
 import com.example.demo.model.User;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,20 +10,21 @@ import java.util.List;
 @Service
 public class UserService {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     public UserService (UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
+    @Transactional
     public void addUser(User user) {
         userRepository.save(user);
     }
-
+    @Transactional
     public void deleteUser(Long id) {
         userRepository.deleteById(id);
     }
-
+    @Transactional
     public void updateUser(User user) {
         userRepository.save(user);
     }
